1bc4108987956e7f492b4e56619cdaea7491a151
Clement Raux <ilesquafr@gmail.com>
2024-07-30 10:12:38 +0200

    récupération des projets dynamiquement et suppresion des boutons (à récupérer dynamiquement aussi)

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -1,35 +1,51 @@
-// **** Ajout de la partie pour filtrer les projets ******* //
+// Récupération dynamique des works  
+
+
+async function fetchWorks() {
+    const response = await fetch('http://localhost:5678/api/works');
+    return await response.json();
+}
+
+async function displayWorks(){
+    const works = await fetchWorks();
+
+    for(const work of works){
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figCaption = document.createElement("figcaption");
+        const gallery = document.getElementById("gallery")
+
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figCaption.innerText = work.title;
+
+        gallery.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+    }
+}
+
+
+// Ajout de la fonction displayWorks après le chargement de la page HTML
+// Pour l'actualisation de tout les projets quand on ajoutera ou supprimera d'autres projets.
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await displayWorks();
+})
+
+
+// **** Ajout de la partie pour filtre les projets ******* //
 
 
 const filtresProjets = document.getElementById("filtresProjets");
 const divFiltre = document.createElement("div");
-
-// Au début utilisation de AppenChild mais la div se retrouvait dans le h2 donc autre méthode
-// Pour placer précisement la div en dessous du h2
-
 filtresProjets.insertAdjacentElement("afterend", divFiltre);
 
 // Création des boutons de filtres 
 
 const boutonTous = document.createElement("button");
 boutonTous.textContent = "Tous";
+boutonTous.addEventListener("click", function(){
 
-
-const boutonObjets = document.createElement("button");
-boutonObjets.textContent = "Objets";
-
-
-const boutonAppartement = document.createElement("button");
-boutonAppartement.textContent = "Appartement";
-
-
-const boutonHotel = document.createElement("button");
-boutonHotel.textContent = "Hotels & restaurants";
-
+});
 divFiltre.appendChild(boutonTous);
-divFiltre.appendChild(boutonObjets);
-divFiltre.appendChild(boutonAppartement);
-divFiltre.appendChild(boutonHotel);
-
-
-
