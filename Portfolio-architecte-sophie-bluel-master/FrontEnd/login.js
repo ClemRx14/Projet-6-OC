6825c08128d9a5815f2c128abce42366acfae83e
Clement Raux <ilesquafr@gmail.com>
2024-07-31 14:53:07 +0200

    Intégration page Login quasi complète

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
@@ -5,3 +5,13 @@ const loginConnexion = document.getElementById('login');
 loginConnexion.addEventListener("click", function() {
     window.location.href = 'login.html';
 });
+
+
+// Retour à la page index.html contenant les projets 
+
+
+const retourIndex = document.getElementById('retourProjets');
+
+retourIndex.addEventListener("click", function() {
+    window.location.href = 'index.html';
+});
